d2f46d28bc19829bbee742b93bc8078875356772
Your Name <github1504@gmail.com>
2024-01-15 22:26:27 +0530

    Verification model UI

--- app/components/auth/Verification.tsx
+++ app/components/auth/Verification.tsx
@@ -13,7 +13,7 @@ type VerifyNumber = {
     "3": string;
 }
 
-const Verification:FC<Props> = ({setRoute}) => {
+const Verification: FC<Props> = ({ setRoute }) => {
     const [invalidError, setInvalidError] = useState(false)
     const inputRefs = [
         useRef<HTMLInputElement>(null),
@@ -21,7 +21,7 @@ const Verification:FC<Props> = ({setRoute}) => {
         useRef<HTMLInputElement>(null),
         useRef<HTMLInputElement>(null),
     ]
-    
+
     const [verifyNumber, setVerifyNumber] = useState<VerifyNumber>({
         "0": "",
         "1": "",
@@ -39,71 +39,71 @@ const Verification:FC<Props> = ({setRoute}) => {
             ...verifyNumber,
             [index]: value
         })
-        if(value === "" && index > 0) {
+        if (value === "" && index > 0) {
             inputRefs[index - 1].current?.focus()
         } else if (value.length === 1 && index < 3) {
             inputRefs[index + 1].current?.focus()
         }
     }
-  return (
-    <div>
-        <h1 className={`${styles.title}`}>
-            Verify Your Account
-        </h1>
-        <br />
-        <div className="w-full flex items-center justify-center mt-2">
+    return (
+        <div>
+            <h1 className={`${styles.title}`}>
+                Verify Your Account
+            </h1>
+            <br />
+            <div className="w-full flex items-center justify-center mt-2">
 
-            <div className='w-[80px] h-[80px] rounded-full bg-[#497Df2] flex items-center justify-center'>
+                <div className='w-[80px] h-[80px] rounded-full bg-[#497Df2] flex items-center justify-center'>
 
-                <VscWorkspaceTrusted size={40} />
+                    <VscWorkspaceTrusted size={40} />
+
+                </div>
 
             </div>
 
-        </div>
+            <br />
 
-        <br />
+            <div className='m-auto flex items-center justify-around'>
 
-        <div className='m-auto flex items-center justify-around'>
+                {
+                    Object.keys(verifyNumber).map((key, index) => {
+                        return (
+                            <input
+                                key={key}
+                                ref={inputRefs[index]}
+                                type="number"
+                                maxLength={1}
+                                placeholder=''
+                                className={`w-[65px] h-[65px] bg-transparent border-[3px] rounded-[10px] flex items-center text-black dark:text-white justify-center text-[18px] font-Poppins outline-none text-center ${invalidError ? 'shake border-red-500' : 'dark:border-white border-[#0000004a]'}`}
+                                value={verifyNumber[key as keyof VerifyNumber]}
+                                onChange={(e) => handleInputChange(index, e.target.value)}
+                            />
+                        )
 
-            {
-                Object.keys(verifyNumber).map((key, index) => {
-                    return (
-                        <input
-                            key={key}
-                             ref={inputRefs[index]}
-                            type="text"
-                            maxLength={1}
-                            placeholder=''
-                            className={`w-[65px] h-[65px] bg-transparent border-[3px] rounded-[10px] flex items-center text-black dark:text-white justify-center text-[18px] font-Poppins outline-none text-center ${invalidError ? 'shake border-red-500' : 'dark:border-white border-[#0000004a]'}`}
-                            value={verifyNumber[key as keyof VerifyNumber]}
-                            onChange={(e) => handleInputChange(index, e.target.value)}
-                        />
-                    )
-                
-                })
-            }
+                    })
+                }
 
-        </div>
+            </div>
 
-        <br />
+            <br />
 
-        <div className='w-full flex justify-center'>
-            <button onClick={VerificationHandler} className={`${styles.button}`}>
-                Verify OTP
-            </button>
-        </div>
+            <div className='w-full flex justify-center'>
+                <button onClick={VerificationHandler} className={`${styles.button}`}>
+                    Verify OTP
+                </button>
+            </div>
 
-        <br />
+            <br />
 
-        <h5 className='text-center pt-2 font-Poppins text-[14px] text-black dark:text-white'>
-            Go back to sign in? 
-            <span onClick={() => setRoute("Login")} className='text-[#2190ff] pl-1 cursor-pointer'>
-                Sign in
+            <h5 className='text-center pt-2 font-Poppins text-[14px] text-black dark:text-white'>
+                Go back to sign in?
+                <span onClick={() => setRoute("Login")} className='text-[#2190ff] pl-1 cursor-pointer'>
+                    Sign in
                 </span>
-        </h5>
+            </h5>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Verification
