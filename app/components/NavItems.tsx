ee4315d62f7913a564158fee69ebfbd4899697aa
Your Name <github1504@gmail.com>
2024-01-15 13:36:58 +0530

    first commit

---
+++ app/components/NavItems.tsx
@@ -0,0 +1,80 @@
+import Link from 'next/link';
+import React from 'react'
+
+export const navItemsData = [
+    {
+        name: "Home",
+        url: '/'
+    },
+    {
+        name: "About",
+        url: '/about'
+    },
+    {
+        name: "Courses",
+        url: '/courses'
+    },
+    {
+        name: "FAQ",
+        url: '/faq'
+    },
+    {
+        name: "Policy",
+        url: '/policy'
+    }
+]
+
+type Props = {
+    activeItem: number;
+    isMobile: boolean;
+}
+
+const NavItems: React.FC<Props> = ({ activeItem, isMobile }) => {
+    return (
+        <>
+            <div className="hidden 800px:flex">
+                {
+                    navItemsData && navItemsData.map((item, index) => {
+                        return (
+                            <Link href={`${item.url}`} key={index} passHref>
+                                <span className={`${activeItem === index ? "dark:text-[#37a39a] text-[crimson]" : "dark:text-white text-black"} text-[18px] px-6 font-Poppins font-[400] `}>
+                                    {item.name}
+                                </span>
+                            </Link>
+                        )
+                    })
+                }
+            </div>
+
+            {
+                isMobile && (
+                    <div className="800px:hidden mt-5">
+
+                        <div className='w-full text-center py-6'>
+
+                            <Link href={'/'} passHref>
+                                <span className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}>
+                                    Leraning
+                                </span>
+                            </Link>
+                        </div>
+
+                        {
+                            navItemsData && navItemsData.map((item, index) => {
+                                return (
+                                    <Link href={`${item.url}`} key={index} passHref>
+                                        <span className={`${activeItem === index ? "dark:text-[#37a39a] text-[crimson]" : "dark:text-white text-black"} block py-5 text-[18px] px-6 font-poopins font-[400] `}>
+                                            {item.name}
+                                        </span>
+                                    </Link>
+                                )
+                            })
+                        }
+                    </div>
+                )
+            }
+        </>
+    )
+}
+
+export default NavItems
